94e2a8847fc521d2285abeef859efa3b7b7cdf75
AhmadShaltoni <mr_shaltoni@icloud.com>
2024-01-05 21:22:59 +0300

    create route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
-import Home from './Home/Home.js';
-
+import { BrowserRouter , Route,Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Profile from './pages/profile/Profile.jsx';
+import SignIn from './pages/signIn/SignIn.jsx';
+import SignUp from './pages/SignUp/SignUp.jsx';
+import ForgetPassword from './pages/ForgetPassword/ForgetPassword.jsx';
+import Offers from './pages/offers/Offers.jsx';
 function App() {
   return (
 
-       <Home />
+      <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home />}/>
+         <Route path="/Profile" element={<Profile />}/>
+         <Route path="/signin" element={<SignIn />}/>
+         <Route path="/signUp" element={<SignUp />}/>
+         <Route path="/ForgetPassword" element={<ForgetPassword />}/>
+         <Route path="/Offers" element={<Offers />}/>
+         
+      </Routes>
+      </BrowserRouter>
 
   );
 }
